875d1de8f385eb82ce47d6f0f29e5689f948d94a
Kazuto Kusama <jacopen@gmail.com>
2022-03-20 09:54:21 +0000

    fix

--- clusters/corp_a/compute.tf
+++ clusters/corp_a/compute.tf
@@ -33,8 +33,8 @@ data "azurerm_resource_group" "main" {
 module "aks" {
   source                           = "Azure/aks/azurerm"
   resource_group_name              = data.azurerm_resource_group.main.name
-  kubernetes_version               = "1.19.3"
-  orchestrator_version             = "1.19.3"
+  kubernetes_version               = "1.23.3"
+  orchestrator_version             = "1.23.3"
   prefix                           = "k8saas"
   cluster_name                     = "corp_a"
   network_plugin                   = "azure"
